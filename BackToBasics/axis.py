19cc6527e6d97b5ec5746252a69cc4d1a1833bca
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2009-01-26 21:25:09 +0000

    Sorted out the axis rendering code. Dots now default to 1 pixel.

--- BackToBasics/axis.py
+++ BackToBasics/axis.py
@@ -4,23 +4,28 @@ width = 300
 height = 300
 size(width, height)
 
-def dot(x, y, dot_size = 3):
-    oval(x - dot_size/2, y - dot_size/2, dot_size, dot_size)
+def dot(x, y, dot_size = 1):
+    #oval(x - dot_size/2, y - dot_size/2, dot_size, dot_size)
+    oval(x, y, dot_size, dot_size)
+
 
 def draw_axis():
     stroke(0.2)
     strokewidth(2)
-    line(0, height/2, width, height/2)
-    line(width/2, 0, width/2, height)    
+    line(0, -height/2, 0, height)
+    line(-width/2, 0, width, 0) # x-axis    
     
-
-draw_axis()
 
 # just some dots to show the old co-od space...
 dot(0,0)
 dot(width-3, height-3)
 
 translate(width/2, height/2)
-
+draw_axis()
+    
 stroke(1.0, 0.0, 0.5)
-dot(0, 60)
+
+for x in range(-width/2, width/2, 3):
+    y = x
+    y *= -1 # flip y co-ods to get standard cartesian axis
+    dot(x, y)

f79335b776bfca7563d68eb63dd0290bc1a7b625
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2009-01-28 01:06:51 +0000

    A bit more progress. Got bar rendering aligned with the text. Still need to sort out margins.

--- BackToBasics/bar_graph.py
+++ BackToBasics/bar_graph.py
@@ -1,9 +1,15 @@
 
-ox = 10
-str = "Arbitary Text"
-str_width = textwidth(str)
-str_height = textheight(str) 
-text(str, ox, 24)
+def graph(str, ox, oy):
+    str_width = textwidth(str)
+    str_height = textheight(str) 
+    text(str, ox, oy + str_height)
 
-gap = 10
-rect(ox + str_width + gap, 0, str_width, str_height)
+    gap = 10
+    bar_height = 10
+    
+    bar_x = ox + str_width + gap 
+    bar_y = oy + str_height 
+    rect(bar_x, bar_y, str_width, -str_height/2)
+
+graph("Strawberry", 0, 0)
+graph("Apple", 0, 40)

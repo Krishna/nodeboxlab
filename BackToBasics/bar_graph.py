a2816020c82456e051534f831e7fa09e00822dfe
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2009-01-28 01:19:30 +0000

    More refinements. Obviously I need to totally review the approach here. Good thing this is just some fun hacking.

--- BackToBasics/bar_graph.py
+++ BackToBasics/bar_graph.py
@@ -7,9 +7,11 @@ def graph(str, ox, oy):
     gap = 10
     bar_height = 10
     
-    bar_x = ox + str_width + gap 
+    bar_x = max([ox + str_width + gap, 130])
     bar_y = oy + str_height 
     rect(bar_x, bar_y, str_width, -str_height/2)
 
-graph("Strawberry", 0, 0)
-graph("Apple", 0, 40)
+graph("Strawberry", 10, 0)
+graph("Apple", 10, 40)
+graph("Pears", 10, 80)
+graph("Markov Chains", 10, 120)
